8a838f8b8bbdb0e728a60ad0ef47d0204ed0d0b2
thsttoan <letrungtoanxautrai@gmail.com>
2025-06-03 19:05:32 +0700

    update sidebar

---
+++ Controller/DuplicateCheckController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QBCA.Controllers
+{
+    public class DuplicateController : Controller
+    {
+        // GET: /Duplicate/CheckDuplicate
+        [HttpGet]
+        public IActionResult CheckDuplicate()
+        {
+            return View();
+        }
+
+        // POST: /Duplicate/CheckDuplicate
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CheckDuplicate(string questionText)
+        {
+            if (string.IsNullOrWhiteSpace(questionText))
+            {
+                ViewBag.Error = "Please enter an English question to check.";
+                return View();
+            }
+
+            // TODO: Gọi API Python tại đây và nhận kết quả
+            // Hiện tại chỉ giả lập kết quả
+            ViewBag.Result = "Duplicate check completed! (This is a mock result.)";
+
+            return View();
+        }
+    }
+}
--- obj/Debug/net8.0/CNPM-QBCA.AssemblyInfo.cs
+++ obj/Debug/net8.0/CNPM-QBCA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNPM-QBCA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b90b7ae78bb739376400986b44f4bc1d617db622")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8218945590a44fa48fcc0d0a75ca2134d6270ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNPM-QBCA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNPM-QBCA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
