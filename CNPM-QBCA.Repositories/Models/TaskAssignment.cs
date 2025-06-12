9b46b43cbd74a737c5dc5588f02ba4e59cd58b2e
mnguyt25 <nguyethtm3590@ut.edu.vn>
2025-06-13 00:25:48 +0700

    Update TaskAssignment.cs
    Signed-off-by: mnguyt25 <nguyethtm3590@ut.edu.vn>

--- CNPM-QBCA.Repositories/Models/TaskAssignment.cs
+++ CNPM-QBCA.Repositories/Models/TaskAssignment.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace QBCA.Models
@@ -7,34 +7,32 @@ namespace QBCA.Models
     {
         [Key]
         public int AssignmentID { get; set; }
-
         // Tham chiếu đến kế hoạch đề
+        [Required]
         public int ExamPlanID { get; set; }
         public ExamPlan ExamPlan { get; set; }
-
+        // Tham chiếu đến phân phối đề
+        [Required]
+        public int? DistributionID { get; set; }
+        public ExamPlanDistribution Distribution { get; set; }
         // Ai giao nhiệm vụ
         public int AssignedBy { get; set; }
         public User Assigner { get; set; }
-
         // Ai nhận nhiệm vụ
         [Required]
         public int AssignedTo { get; set; }
         public User Assignee { get; set; }
-
         // Vai trò (ví dụ: Reviewer, Creator, Manager)
         public string Role { get; set; }
-
         // Loại công việc
         [Required]
         public string TaskType { get; set; }
-
         // Mô tả nhiệm vụ
+        [Required]
         public string Description { get; set; }
-
         // Trạng thái nhiệm vụ
         [Required]
         public string Status { get; set; }
-
         public DateTime AssignedAt { get; set; }
         public DateTime? DueDate { get; set; }
         public DateTime CreatedAt { get; set; }
