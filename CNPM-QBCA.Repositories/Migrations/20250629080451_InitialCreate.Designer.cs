beb0c41e8fa5a8680d969300db0838fae841dfc8
thsttoan <letrungtoanxautrai@gmail.com>
2025-06-29 15:30:35 +0700

    fixbug

--- CNPM-QBCA.Repositories/Migrations/20250629080451_InitialCreate.Designer.cs
+++ CNPM-QBCA.Repositories/Migrations/20250629080451_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using QBCA.Data;
 namespace CNPM_QBCA.Repositories.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250628192705_InitialCreate")]
+    [Migration("20250629080451_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
